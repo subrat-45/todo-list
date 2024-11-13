771c8fae1e5116ed5723d93d3a1f6971fdbe7f8e
subrat45 <subrat.kumar22042005@gmail.com>
2024-11-13 15:06:11 +0530

    First react project

--- app/page.js
+++ app/page.js
@@ -1,101 +1,66 @@
-import Image from "next/image";
+"use client";
+import React, { forwardRef, useState } from "react";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+export default function TodoApp() {
+  const [text, setText] = useState("");
+  const [desc, setDesc] = useState("");
+  const [mainTask, setMainTask] = useState([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  function reloadHandler(e) {
+    e.preventDefault();
+    setMainTask([...mainTask, { text, desc }]);
+    setText("");
+    setDesc("");
+  }
+
+  let showTask = <h2>No Task Available</h2>;
+  if (mainTask.length > 0) {
+    showTask = mainTask.map((task, i) => (
+      <li key={i} className="mb-5 flex">
+        <div className="flex justify-evenly items-center w-2/3">
+          <h5 className="text-2xl font-serif">{task.text}</h5>
+          <h5 className="text-2xl font-serif">{task.desc}</h5>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <button onClick={() => {
+          let referTask = [...mainTask];
+          referTask.splice(i,1);
+          setMainTask(referTask);
+        }} className="bg-red-500 text-center text-white p-2.5 rounded-md font-bold">Delete</button>
+      </li>
+    ));
+  }
+
+  return (
+    <>
+      <div className="text-center flex-wrap">
+        <h1 className="bg-black text-cyan-400 p-5 text-center font-bold text-5xl shadow-2xl">
+          TODO APP
+        </h1>
+        <form onSubmit={reloadHandler}>
+          <input
+            type="text"
+            placeholder="Enter text here"
+            className="border-2 border-black m-5 p-2 rounded-md"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <input
+            type="text"
+            placeholder="Enter Description"
+            className="border-2 border-black m-5 p-2 rounded-md"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <button className="bg-black text-cyan-400 p-2 m-5 rounded-md border-2">
+            Confirm
+          </button>
+        </form>
+      
+
+        <div className="bg-slate-300 p-7">
+          <ul>{showTask}</ul>
+        </div>
+      </div>
+    </>
   );
 }
